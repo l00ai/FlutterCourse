c0516b5d22490c9a63b9d83c5c60710f02b7904e
l00ai <74601007+l00ai@users.noreply.github.com>
2023-06-06 10:58:46 +0300

    BottomSheet&Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,163 @@
-import 'dart:ffi';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
 
   runApp(
     MaterialApp(
-      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+      home: MyApp2(),
     ),
   );
 }
+
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff2E466E) ,
+      appBar: AppBar(
+        title: Text("MUSICLIST"),
+        centerTitle: true,
+        backgroundColor: Color(0xff2E466E),
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.grid_view_rounded)),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            children: [
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+              ListItem(),
+
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+class ListItem extends StatefulWidget {
+  const ListItem({Key? key}) : super(key: key);
+
+  @override
+  State<ListItem> createState() => _ListItemState();
+}
+class _ListItemState extends State<ListItem> {
+
+  bool _value  = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            height: 100,
+            width: double.maxFinite,
+            margin: EdgeInsets.only(left: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.white.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      offset: Offset(-3, 10)
+                  )
+                ]
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 70),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                   Text("Title", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22, color:  Color(0xff2E466E)),),
+                   Text("body", style: TextStyle(color: Color(0xff2E466E)),),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: -20,
+            left: -10,
+            child: Container(
+              width: 100,
+              height:100,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 6,
+                        spreadRadius: 6,
+                        offset: Offset(0, 10)
+                    )
+                  ]
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+
+              ),
+            ),
+          ),
+          Positioned(
+            top: -15,
+            left: -25,
+            child: Theme(
+              data: Theme.of(context).copyWith(
+                unselectedWidgetColor: Colors.white,
+              ),
+              child: Checkbox(
+                activeColor: Colors.white,
+                checkColor: Colors.black,
+                onChanged: (value){
+                  setState(() {
+                    _value = value ?? false;
+                  });
+                },
+                value: _value,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+// BottomSheet
+// RoundedRectangleBorder
+// DraggableScrollableSheet
+
+
+
+// 1- Alert Dialog
+// 2- Custom Dialog
+// 3- Full-Screen Dialog
 
 
 
@@ -23,234 +171,155 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int count = 0 ;
 
 
-  @override
-  void initState() {
-    super.initState();
-    print("initState");
+  void _settingModalBottomSheet(context){
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        isDismissible: false,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+        ),
+        builder: (BuildContext bc){
+          return DraggableScrollableSheet(
+              initialChildSize: 0.5,
+              minChildSize: 0.4,
+              maxChildSize: 0.6,
+              expand: false,
+            builder: (context, scrollController) {
+              return ListView(
+                  controller: scrollController,
+                  children: <Widget>[
+                    ListTile(
+                        leading: Icon(Icons.music_note),
+                        title: Text('Music'),
+                        onTap: () => {}
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.videocam),
+                      title: Text('Video'),
+                      onTap: () => {},
+                    ),
+                  ],
+              );
+            }
+          );
+        }
+    );
+  }
+
+  void _settingModalAlertDialog(BuildContext context){
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)), //this right here
+            child: Container(
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Alert Dialog"),
+                    SizedBox(
+                      width: 320.0,
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Save",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: const Color(0xFF1BC0C5),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+    //  showDialog(
+    //     context: context,
+    //     builder: (BuildContext context){
+    //       return AlertDialog(
+    //         title: Text("Alert Dialog"),
+    //         content: Text("Dialog Content"),
+    //         actions:[
+    //           MaterialButton(
+    //             onPressed: () { Navigator.pop(context); },
+    //             child: Text("Close"),
+    //             color: Colors.red,
+    //           )
+    //         ],
+    //
+    //       );
+    //     }
+    // );
+  }
+
+  void _settingFullAlertDialog(BuildContext context){
+    showGeneralDialog(
+        context: context,
+        barrierDismissible: true,
+        barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+        barrierColor: Colors.black45,
+        transitionDuration: const Duration(milliseconds: 200),
+        pageBuilder: (BuildContext context, Animation animation, Animation secondaryAnimation) {
+          return Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width - 10,
+              height: MediaQuery.of(context).size.height -  80,
+              padding: EdgeInsets.all(20),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      "Save",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: const Color(0xFF1BC0C5),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
 
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose");
-  }
-
-
-  bool checkBoxValue = false;
 
   @override
   Widget build(BuildContext context) {
-    print("build");
-    // double width = MediaQuery.of(context).size.width;
-    // double height = MediaQuery.of(context).size.height;
-    //
-    // print("W : $width");
-    // print("H : $height");
 
     return Scaffold(
       appBar: AppBar(
         title: Text("This is app bar"),
         centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: (){},
-            icon: Icon(Icons.add),
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(
-              padding: EdgeInsets.zero,
-              child: Container(
-                color: Colors.green,
-              ),
-            ),
-            ListTile(
-              title: Text("Title"),
-              subtitle: Text("Sub Title"),
-              leading: Icon(Icons.supervised_user_circle),
-              trailing: Icon(Icons.more_horiz_rounded),
-              onTap: (){},
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          //_settingModalBottomSheet(context);
+          // _settingModalAlertDialog(context);
+           _settingFullAlertDialog(context);
+        },
+        child: Icon(Icons.add),
       ),
+
       body: Column(
         children: [
-          Container(),
-          Expanded(
-            child: Container(
-              color: Colors.green,
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Number is : $count",
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22),
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Checkbox(
-                        value: checkBoxValue,
-                        onChanged: (value) {
-                          setState(() {
-                            checkBoxValue = value ?? checkBoxValue;
-                          });
-                        },
-                      ),
-                      Text("Check box")
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  MyCustomButton(
-                    label: "Plus",
-                    onClicked: (){
-                      setState(() {
-                        count++;
-                        print(count);
-                      });
-                    },
-                  ),
-                  SizedBox(height: 10,),
-                  Stack(
-                    clipBehavior: Clip.none,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.deepOrange,
-                      ),
-                      Positioned(
-                        bottom: -30,
-                        left: -30,
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.lightGreenAccent,
-                        ),
-                      ),
-                    ],
-                  )
 
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                        color: Colors.purpleAccent,
-                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child: Column(
-                          children: [
-                            Expanded(
-                                flex: 2,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(15),
-                                    border: Border.all(color: Colors.white),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.white.withOpacity(0.5),
-                                        blurRadius: 5,
-                                        spreadRadius: 5,
-                                        offset: Offset(-5, 5)
-                                      )
-                                    ]
-                                  ),
-                                ),
-                            ),
-                            SizedBox(height: 10,),
-                            Expanded(
-                                flex: 3,
-                                child: Container(color: Colors.indigoAccent)
-                            ),
-                          ],
-                        ),
-                      ),
-                  ),
-                  Expanded(
-                      child: SingleChildScrollView(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: Column(
-                            children: [
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                              Container(height: 40, color: Colors.white,),
-                              SizedBox(height: 10,),
-                            ],
-                          ),
-                        ),
-                      ),
-                  ),
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
   }
 }
-
-
-
-typedef MyFunc = void Function()?;
-
-class MyCustomButton extends StatelessWidget {
-  final String label;
-  final MyFunc? onClicked;
-  const MyCustomButton({Key? key, required this.label, this.onClicked}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        onTap: onClicked,
-        child: Ink(
-          width: 100,
-          height: 50,
-          color: Colors.white,
-          child: Center(child: Text(label, style: TextStyle(color: Colors.black, fontSize: 18) ,)),
-        ),
-      ),
-    );
-  }
-}
-
-
-
 
 
 
