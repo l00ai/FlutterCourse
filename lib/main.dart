74326ec309b9df62c10d802b56bf9e36d3db8a95
l00ai <74601007+l00ai@users.noreply.github.com>
2023-06-13 14:04:38 +0300

    TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
-
   runApp(
       MaterialApp(
        debugShowCheckedModeBanner: false,
@@ -15,79 +11,83 @@ void main(){
 }
 
 
-
-
-// ListView
-// 1- ListView
-// 2- ListView.Builder
-// 2- ListView.separated
-
-
-
-
-
-// GridView
-// #1- GridView          // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-// #2- GridView.builder  // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-
-// 3- GridView.count
-// 4- GridView.extent //double width = MediaQuery.of(context).size.width;
-
-
-
-
-
-
 class MyApp extends StatefulWidget {
   final String name;
-  const MyApp({super.key, required this.name});
+  MyApp({super.key, required this.name});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
 
-  List<String> empNameList = [
-    "Ahmed",
-    "Ali",
-    "Omar",
-    "Sami",
-    "Esam",
-    "Mohammed",
-  ];
 
+  late TabController controller;
+
+  tabListener() {
+    print(controller.index);
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
+   controller = TabController(length: 3, vsync: this);
+   controller.addListener(tabListener);
     super.initState();
   }
+
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    controller.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.name),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.pink,
-                Colors.pink.shade900
-              ],
-            )
-          ),
+        title: const Text("Title"),
+        actions: [
+          TextButton(
+              onPressed: (){
+                //controller.animateTo(2);
+              },
+              child: const Text("Next", style: TextStyle(color: Colors.white),)
+          )
+        ],
+        bottom: TabBar(
+          controller: controller,
+          tabs: const [
+            Tab(
+              text: "Home",
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              text: "Search",
+              icon: Icon(Icons.search),
+            ),
+            Tab(
+              text: "Profile",
+              icon: Icon(Icons.person),
+            ),
+          ],
         ),
       ),
-      drawer: Drawer(),
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        children: const [
-          ListItem(),
-          ListItem(),
-          ListItem(),
-          ListItem(),
+      body: TabBarView(
+        //physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          Container(
+            color: Colors.purpleAccent,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.indigoAccent,
+          ),
         ],
       )
     );
@@ -95,78 +95,133 @@ class _MyAppState extends State<MyApp> {
 }
 
 
-class ListItem extends StatelessWidget {
-  const ListItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Header 1"),
-          const SizedBox(height: 10,),
-          GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-                mainAxisExtent: height * 0.25
-            ),
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            children: const [
-              GridItem(),
-              GridItem(),
-              GridItem(),
-              GridItem(),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 
 
-class GridItem extends StatelessWidget {
-  const GridItem({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.pinkAccent
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const Expanded(
-            child: Center(child: Icon(Icons.ac_unit, color: Colors.white,)),
-          ),
-          Container(
-            height: 35,
-            decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.2),
-                borderRadius: const BorderRadius.vertical(bottom: Radius.circular(10))
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            width: double.maxFinite,
-            alignment: Alignment.centerLeft,
-            child: const Text(
-              "Title",
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
+
+
+
+// class MyApp extends StatefulWidget {
+//   final String name;
+//   MyApp({super.key, required this.name});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
+//
+//   late TabController tabController;
+//
+//   @override
+//   void initState() {
+//     tabController = TabController(length: 3, vsync: this);
+//     super.initState();
+//   }
+//
+//   @override
+//   void dispose() {
+//     tabController.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text("TabBar & TabBar View"),
+//           bottom: TabBar(
+//             controller: tabController,
+//             tabs: const [
+//               Tab(
+//                 text: 'Gift',
+//                 icon: Icon(
+//                   Icons.card_giftcard,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//               Tab(
+//                 text: 'Ac',
+//                 icon: Icon(
+//                   Icons.ac_unit_rounded,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//               Tab(
+//                 text: 'Adb',
+//                 icon: Icon(
+//                   Icons.adb,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           controller: tabController,
+//           children: [
+//             Container(color: Colors.pink,),
+//             Container(color: Colors.purpleAccent,),
+//             Container(color: Colors.blueAccent,),
+//           ],
+//         )
+//     );
+//   }
+// }
+
+
+// class MyApp extends StatelessWidget {
+//   final String name;
+//   MyApp({super.key, required this.name});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 3,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Text"),
+//           bottom: const TabBar(
+//             tabs: [
+//               Tab(
+//                 text: 'Gift',
+//                 icon: Icon(
+//                   Icons.card_giftcard,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//               Tab(
+//                 text: 'Ac',
+//                 icon: Icon(
+//                   Icons.ac_unit_rounded,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//               Tab(
+//                 text: 'Adb',
+//                 icon: Icon(
+//                   Icons.adb,
+//                 ),
+//                 iconMargin: EdgeInsets.only(bottom: 10.0),
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             Container(color: Colors.pink,),
+//             Container(color: Colors.purpleAccent,),
+//             Container(color: Colors.blueAccent,),
+//           ],
+//         )
+//       ),
+//     );
+//   }
+// }
+//
+//
+
 
 
 
