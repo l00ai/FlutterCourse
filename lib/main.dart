db7db13c8c6b38860b4cae15ad4712db47a0c691
l00ai <74601007+l00ai@users.noreply.github.com>
2023-06-18 10:04:29 +0300

    Assets and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,231 +1,170 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(
-      MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: MyApp(name: "Title"),
-      )
-  );
+void main() {
+  runApp(const MaterialApp(
+    title: 'Bottom NavBar',
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
 
-class MyApp extends StatefulWidget {
-  final String name;
-  MyApp({super.key, required this.name});
+// Icons.home_filled,
+// Icons.home_outlined,
+// Icons.work_rounded,
+// Icons.work_outline_outlined,
+// Icons.widgets_rounded,
+// Icons.widgets_outlined,
+// Icons.person,
+// Icons.person_outline,
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> {
 
+  int index = 0;
 
-  late TabController controller;
-
-  tabListener() {
-    print(controller.index);
-  }
-
-  @override
-  void initState() {
-   controller = TabController(length: 3, vsync: this);
-   controller.addListener(tabListener);
-    super.initState();
-  }
-
+  List<Widget> pages = [
+    const HomeScreen(),
+    Container(color: Colors.pink),
+    Container(color: Colors.green),
+    Container(color: Colors.red),
+  ];
 
   @override
-  void dispose() {
-    controller.removeListener(tabListener);
-    controller.dispose();
-    super.dispose();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Title"),
-        actions: [
-          TextButton(
+        title: const Text("Bottom nav bar"),
+      ),
+      bottomNavigationBar: Container(
+        height: 52,
+        decoration: const BoxDecoration(
+          color: Colors.blue
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+                onPressed: (){
+                  setState(() {
+                    index = 0;
+                  });
+                },
+                icon: Icon(Icons.home_outlined, color: index == 0 ? Colors.white : null),
+            ),
+            IconButton(
               onPressed: (){
-                //controller.animateTo(2);
+                setState(() {
+                  index = 1;
+                });
               },
-              child: const Text("Next", style: TextStyle(color: Colors.white),)
-          )
-        ],
-        bottom: TabBar(
-          controller: controller,
-          tabs: const [
-            Tab(
-              text: "Home",
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.work_outline_outlined, color: index == 1 ? Colors.white : null),
             ),
-            Tab(
-              text: "Search",
-              icon: Icon(Icons.search),
+            IconButton(
+              onPressed: (){
+                setState(() {
+                  index = 2;
+                });
+              },
+              icon: Icon(Icons.widgets_outlined, color: index == 2 ? Colors.white : null),
             ),
-            Tab(
-              text: "Profile",
-              icon: Icon(Icons.person),
+            IconButton(
+              onPressed: (){
+                setState(() {
+                  index = 3;
+                });
+              },
+              icon: Icon(Icons.person_outline, color: index == 3 ? Colors.white : null),
             ),
           ],
         ),
       ),
-      body: TabBarView(
-        //physics: const NeverScrollableScrollPhysics(),
-        controller: controller,
-        children: [
-          Container(
-            color: Colors.purpleAccent,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-          Container(
-            color: Colors.indigoAccent,
-          ),
-        ],
-      )
+      body: pages[index],
     );
   }
 }
 
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
 
+    TextStyle style = const TextStyle(
+      color: Colors.white,
+      fontSize: 18,
+      fontFamily: "Cairo",
+      fontWeight: FontWeight.normal
+    );
 
-
-
-
-
-// class MyApp extends StatefulWidget {
-//   final String name;
-//   MyApp({super.key, required this.name});
-//
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
-//
-//   late TabController tabController;
-//
-//   @override
-//   void initState() {
-//     tabController = TabController(length: 3, vsync: this);
-//     super.initState();
-//   }
-//
-//   @override
-//   void dispose() {
-//     tabController.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text("TabBar & TabBar View"),
-//           bottom: TabBar(
-//             controller: tabController,
-//             tabs: const [
-//               Tab(
-//                 text: 'Gift',
-//                 icon: Icon(
-//                   Icons.card_giftcard,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//               Tab(
-//                 text: 'Ac',
-//                 icon: Icon(
-//                   Icons.ac_unit_rounded,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//               Tab(
-//                 text: 'Adb',
-//                 icon: Icon(
-//                   Icons.adb,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//             ],
-//           ),
-//         ),
-//         body: TabBarView(
-//           controller: tabController,
-//           children: [
-//             Container(color: Colors.pink,),
-//             Container(color: Colors.purpleAccent,),
-//             Container(color: Colors.blueAccent,),
-//           ],
-//         )
-//     );
-//   }
-// }
-
-
-// class MyApp extends StatelessWidget {
-//   final String name;
-//   MyApp({super.key, required this.name});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: 3,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           title: const Text("Text"),
-//           bottom: const TabBar(
-//             tabs: [
-//               Tab(
-//                 text: 'Gift',
-//                 icon: Icon(
-//                   Icons.card_giftcard,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//               Tab(
-//                 text: 'Ac',
-//                 icon: Icon(
-//                   Icons.ac_unit_rounded,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//               Tab(
-//                 text: 'Adb',
-//                 icon: Icon(
-//                   Icons.adb,
-//                 ),
-//                 iconMargin: EdgeInsets.only(bottom: 10.0),
-//               ),
-//             ],
-//           ),
-//         ),
-//         body: TabBarView(
-//           children: [
-//             Container(color: Colors.pink,),
-//             Container(color: Colors.purpleAccent,),
-//             Container(color: Colors.blueAccent,),
-//           ],
-//         )
-//       ),
-//     );
-//   }
-// }
-//
-//
-
-
-
-
-
-
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.green,
+          ),
+          alignment: Alignment.center,
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+            child: Image.asset(
+                height: 200,
+                //width: MediaQuery.of(context).size.width,
+                //fit: BoxFit.fill,
+                "assets/images/flutter_logo.png"),
+          ),
+        ),
+        const SizedBox(height: 10,),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.green,
+          ),
+          alignment: Alignment.center,
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+            child: Image.network(
+                height: 200,
+                //width: MediaQuery.of(context).size.width,
+                //fit: BoxFit.fill,
+                headers: const {
+                  "Auth": 'token',
+                },
+                loadingBuilder: (context, child, loadingProgress) => Text(loadingProgress.toString()),
+                errorBuilder: (context, error, stackTrace) => Text(stackTrace.toString()),
+                "https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png"),
+          ),
+        ),
+        const SizedBox(height: 10,),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.green,
+          ),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text("هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.", style: style,),
+              const SizedBox(height: 10,),
+              Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa", style: style,)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
 
 
