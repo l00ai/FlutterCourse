a91b22707a1ea2d792d01e3d983f14f59d75a466
l00ai <74601007+l00ai@users.noreply.github.com>
2023-06-19 10:43:34 +0300

    SVG and FORM

--- lib/main.dart
+++ lib/main.dart
@@ -1,170 +1,185 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'Bottom NavBar',
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
-}
+void main() => runApp(
+    MaterialApp(
+      title: "Tutorial",
+      home: MyPage(),
+    ),
+);
 
-
-// Icons.home_filled,
-// Icons.home_outlined,
-// Icons.work_rounded,
-// Icons.work_outline_outlined,
-// Icons.widgets_rounded,
-// Icons.widgets_outlined,
-// Icons.person,
-// Icons.person_outline,
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int index = 0;
-
-  List<Widget> pages = [
-    const HomeScreen(),
-    Container(color: Colors.pink),
-    Container(color: Colors.green),
-    Container(color: Colors.red),
-  ];
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Bottom nav bar"),
+        title: Text("SVG"),
       ),
-      bottomNavigationBar: Container(
-        height: 52,
-        decoration: const BoxDecoration(
-          color: Colors.blue
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-                onPressed: (){
-                  setState(() {
-                    index = 0;
-                  });
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(),
+          SvgPicture.asset(
+              "assets/vectors/light.svg",
+              semanticsLabel: 'Acme Logo',
+            height: 100,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController ageController = TextEditingController();
+
+  late TextEditingController nameController ;
+
+  @override
+  void initState() {
+    nameController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic form"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(10.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: nameController,
+                enabled: true,
+                // obscureText: true,
+                keyboardType: TextInputType.text,
+                //textInputAction: TextInputAction.newline,
+                //maxLines: 4,
+                maxLength: 10,
+                validator: (value){
+                  if(value != null && value.isEmpty){
+                    return "Enter user name!";
+                  }
+                  return null;
                 },
-                icon: Icon(Icons.home_outlined, color: index == 0 ? Colors.white : null),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChanged: (value) => _formKey.currentState!.validate(),
+                decoration: const InputDecoration(
+                  labelText: 'UserName',
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
+                  ),
+                   counterText: "",
+                   hintText: "Please enter username ...",
+                  // border: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  //   borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  // ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide: BorderSide(color: Colors.red, width: 2.0),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderSide:
+                        BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+                  ),
+                ),
+              ),
             ),
-            IconButton(
-              onPressed: (){
-                setState(() {
-                  index = 1;
-                });
+            // Padding(
+            //   padding: const EdgeInsets.all(10.0),
+            //   child: TextFormField(
+            //     controller: ageController,
+            //     validator: (value) {
+            //       if (value!.isEmpty) {
+            //         return 'Please enter age';
+            //       }
+            //       return null;
+            //     },
+            //     keyboardType: TextInputType.number,
+            //     style: TextStyle(
+            //       fontSize: 13,
+            //     ),
+            //     decoration: const InputDecoration(
+            //       labelText: 'Age',
+            //       hintText: "20",
+            //       prefixIcon: Icon(Icons.date_range),
+            //       suffixIcon: Icon(
+            //         Icons.delete_outline,
+            //         color: Colors.red,
+            //       ),
+            //       enabledBorder: OutlineInputBorder(
+            //         borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            //         borderSide: BorderSide(color: Colors.black, width: 2.0),
+            //       ),
+            //       focusedBorder: OutlineInputBorder(
+            //         borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            //         borderSide: BorderSide(color: Colors.blue, width: 2.0),
+            //       ),
+            //       errorBorder: OutlineInputBorder(
+            //         borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            //         borderSide: BorderSide(color: Colors.red, width: 2.0),
+            //       ),
+            //       focusedErrorBorder: OutlineInputBorder(
+            //         borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            //         borderSide:
+            //             BorderSide(color: Colors.deepOrangeAccent, width: 2.0),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            MaterialButton(
+              color: Colors.grey,
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  debugPrint("Name is ${nameController.text}");
+                  debugPrint("Age is ${ageController.text}");
+                }
               },
-              icon: Icon(Icons.work_outline_outlined, color: index == 1 ? Colors.white : null),
-            ),
-            IconButton(
-              onPressed: (){
-                setState(() {
-                  index = 2;
-                });
-              },
-              icon: Icon(Icons.widgets_outlined, color: index == 2 ? Colors.white : null),
-            ),
-            IconButton(
-              onPressed: (){
-                setState(() {
-                  index = 3;
-                });
-              },
-              icon: Icon(Icons.person_outline, color: index == 3 ? Colors.white : null),
+              child: Text("Submit"),
             ),
           ],
         ),
       ),
-      body: pages[index],
     );
   }
 }
-
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-
-    TextStyle style = const TextStyle(
-      color: Colors.white,
-      fontSize: 18,
-      fontFamily: "Cairo",
-      fontWeight: FontWeight.normal
-    );
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color: Colors.green,
-          ),
-          alignment: Alignment.center,
-          child: ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-            child: Image.asset(
-                height: 200,
-                //width: MediaQuery.of(context).size.width,
-                //fit: BoxFit.fill,
-                "assets/images/flutter_logo.png"),
-          ),
-        ),
-        const SizedBox(height: 10,),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color: Colors.green,
-          ),
-          alignment: Alignment.center,
-          child: ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-            child: Image.network(
-                height: 200,
-                //width: MediaQuery.of(context).size.width,
-                //fit: BoxFit.fill,
-                headers: const {
-                  "Auth": 'token',
-                },
-                loadingBuilder: (context, child, loadingProgress) => Text(loadingProgress.toString()),
-                errorBuilder: (context, error, stackTrace) => Text(stackTrace.toString()),
-                "https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png"),
-          ),
-        ),
-        const SizedBox(height: 10,),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color: Colors.green,
-          ),
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Text("هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.", style: style,),
-              const SizedBox(height: 10,),
-              Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa", style: style,)
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-
